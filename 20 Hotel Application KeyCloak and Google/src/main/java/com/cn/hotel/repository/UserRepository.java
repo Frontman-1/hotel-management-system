61ae200d447cce553f1fa67f199af8bd1225aefb
Paila Akhil <akhil.paila89@gmail.com>
2025-12-05 12:50:43 +0530

    Hotel Management System

---
+++ 20 Hotel Application KeyCloak and Google/src/main/java/com/cn/hotel/repository/UserRepository.java
@@ -0,0 +1,8 @@
+//package com.cn.hotel.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import com.cn.hotel.model.User;
+//
+//public interface UserRepository  extends JpaRepository<User, Long> {
+//    User findByEmail(String email);
+//}
---
+++ 20 Hotel Application KeyCloak and Google/src/main/java/com/cn/hotel/service/UserService.java
@@ -0,0 +1,38 @@
+//package com.cn.hotel.service;
+//
+//import java.util.List;
+//import org.springframework.stereotype.Service;
+//import com.cn.hotel.dto.UserRequest;
+//import com.cn.hotel.model.User;
+//import com.cn.hotel.repository.UserRepository;
+//
+//@Service
+//public class UserService {
+//    private final UserRepository userRepository;
+//
+//    public UserService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    public List<User> getUsers() {
+//        return userRepository.findAll();
+//    }
+//
+//    public User getUserById(Long id) {
+//        return userRepository.findById(id).get();
+//    }
+//
+//    public User createUser(UserRequest userRequest) {
+//        User user = new User();
+//        user.setUsername(userRequest.getUsername());
+//        user.setPassword(userRequest.getPassword());
+//        user.setRole(userRequest.getRole());
+//        user.setEmail(userRequest.getEmail());
+//
+//        return userRepository.save(user);
+//    }
+//
+//    public void deleteUserById(Long id) {
+//        userRepository.deleteById(id);
+//    }
+//}
